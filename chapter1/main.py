5dca1f1a0e5170151981b67b76cb4c3fbd4bce4f
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-03-08 23:44:19 +0500

    Creating static URLs

--- chapter1/main.py
+++ chapter1/main.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def index():
     return "This is an online ... counseling system (OPCS)"
 
+@app.route('/home')
+def home():
+    return '''
+        <html><head><title>Online Personal … System</title>
+            </head><body>
+            <h1>Online … Counseling System (OPCS)</h1>
+            <p>This is a template of a web-based counseling
+                application where counselors can … … …</em>
+            </body></html>
+        '''
 
 if __name__ == "__main__":
     app.run(debug=True)
