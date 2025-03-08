563cbfeae87f92c5e977fc4eb94c81095a951c7e
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-03-08 23:53:22 +0500

    Applying path variables

--- chapter1/main.py
+++ chapter1/main.py
@@ -1,12 +1,17 @@
 from flask import Flask
 
+from converter.date_converter import DateConverter
+
 app = Flask(__name__)
+app.url_map.converters['date'] = DateConverter
 
 @app.route('/', methods = ['GET'])
 def index():
     return "This is an online ... counseling system (OPCS)"
 
 @app.route('/home')
+@app.route('/information')
+@app.route('/introduction')
 def home():
     return '''
         <html><head><title>Online Personal … System</title>
@@ -16,6 +21,7 @@ def home():
                 application where counselors can … … …</em>
             </body></html>
         '''
+
 
 if __name__ == "__main__":
     app.run(debug=True)
