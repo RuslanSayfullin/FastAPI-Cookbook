f738515ed57dc76f3c1d36670c3b6ecfecc022ee
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-04-08 18:05:20 +0500

    Add example with sqllite3

---
+++ Chapter02/sql_example/main.py
@@ -0,0 +1,17 @@
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+from database import SessionLocal, User
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
+
+app = FastAPI()
+
+@app.get("/users")
+def read_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
