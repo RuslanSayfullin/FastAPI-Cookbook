4a4d206ce2e49a0162c40a85605fb44abc0a6a39
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-04-08 16:22:37 +0500

    Add first chapter

--- Chapter01/fastapi_start/main.py
+++ Chapter01/fastapi_start/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+import router_example
 
 app = FastAPI()
+app.include_router(router_example.router)
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
