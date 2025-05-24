cd83e7fb1dd3cdc99a4543b6fdf890598e244f3f
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-05-24 22:12:34 +0500

    First Steps with FastAPI

--- Chapter01/bookstore/main.py
+++ Chapter01/bookstore/main.py
@@ -10,6 +10,53 @@ from models import Book
 
 app = FastAPI()
 
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    return {
+        "book_id": book_id,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald"
+    }
+
+@app.get("/authors/{author_id}")
+async def read_author(author_id: int):
+    return {
+        "author_id": author_id,
+        "name": "Ernest Hemingway"
+    }
+
+@app.get("/books")
+async def read_books(year: int = None):
+    if year:
+        return {
+            "year": year,
+            "books": ["Book 1", "Book 2"]
+        }
+    return {"books": ["All Books"]}
+
+@app.post("/book")
+async def create_book(book: Book):
+    return book
+
+class BookResponse(BaseModel):
+    title: str
+    author: str
+
+@app.get("/allbooks")
+async def read_all_books() -> list[BookResponse]:
+    return [
+        {
+            "id": 1,
+            "title": "1984",
+            "author": "George Orwell"
+        },
+        {
+            "id:": 2,
+            "title": "The Great Gatsby",
+            "author": "F. Scott Fitzgerald"
+        },
+    ]
+
 # exceptions handlers
 @app.exception_handler(HTTPException)
 async def http_handler(request, exc):
@@ -39,49 +86,11 @@ async def validation_exception_handler(
 
 
 
-class BookResponse(BaseModel):
-    title: str
-    author: str
 
-@app.get("/allbooks")
-async def read_all_books() -> list[BookResponse]:
-    return [
-        {
-            "id": 1,
-            "title": "1984",
-            "author": "George Orwell"
-        },
-        {
-            "id:": 2,
-            "title": "The Great Gatsby",
-            "author": "F. Scott Fitzgerald"
-        }
-    ]
 
-@app.get("/books/{book_id}")
-async def read_book(book_id: int):
-    return {
-        "book_id": book_id,
-        "title": "The Great Gatsby",
-        "author": "F. Scott Fitzgerald"
-    }
 
-@app.get("/authors/{author_id}")
-async def read_author(author_id: int):
-    return {
-        "author_id": author_id,
-        "name": "Ernest Hemingway"
-    }
 
-@app.get("/books")
-async def read_books(year: int = None):
-    if year:
-        return {
-            "year": year,
-            "books": ["Book 1", "Book 2"]
-        }
-    return {"books": ["All Books"]}
 
-@app.post("/book")
-async def create_book(book: Book):
-    return book
+
+
+
