153d69d4ab8f85f662a8ab677470ae505ca866f9
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2025-07-21 18:26:47 +0500

    Гзвфеу

--- Chapter02/sql_example/database.py
+++ Chapter02/sql_example/database.py
@@ -1,4 +1,10 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL)
 
 
 class Base(DeclarativeBase):
@@ -6,7 +12,13 @@ class Base(DeclarativeBase):
 
 class User(Base):
     __tablename__ = "user"
-    id: Mapped[int] = mapped_column
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str]
 
+Base.metadata.create_all(bind=engine)
 
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
