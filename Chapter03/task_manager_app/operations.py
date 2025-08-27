6bb78bd52feb4ea5cf19611a67faeb13752cb66e
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2025-08-27 18:26:31 +0500

    Create Project

---
+++ Chapter03/task_manager_app/operations.py
@@ -0,0 +1,26 @@
+import csv
+from typing import Optional
+
+from .models import Task, TaskWithID
+
+
+DATABASE_FILENAME = "tasks.csv"
+
+column_fields = [
+    "id", "title", "description", "status"
+]
+
+def read_all_tasks() -> list[TaskWithID]:
+    with open(DATABASE_FILENAME) as csvfile:
+        reader = csv.DictReader(csvfile,)
+
+        return [TaskWithID(**row) for row in reader]
+
+def read_task(task_id) -> Optional[TaskWithID]:
+    with open(DATABASE_FILENAME) as csvfile:
+        reader = csv.DictReader(
+            csvfile,
+        )
+        for row in reader:
+            if int(row["id"]) == task_id:
+                return TaskWithID(**row)
