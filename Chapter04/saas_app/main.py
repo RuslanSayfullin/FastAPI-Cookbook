67200301e41d64a28bb6c23bc73dd5aa1929e585
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2025-09-16 18:27:42 +0500

    Обновил

---
+++ Chapter04/saas_app/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Saas application",
+    description="This is a Saas application",
+    version="0.1.0",
+    )
--- appendix/aiohttp/first_example.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-async def a_square(x):
-    print(f'Asynchronously squaring {x}!')
-
-    return x ** 2
-
-asyncio.run(a_square(2))
