275236c53e8de780aa17169f93ce3f2804b34516
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-09-18 12:15:58 +0500

    First chapter

--- Chapter01/bookstore/main.py
+++ Chapter01/bookstore/main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware 
+from pydantic import BaseModel
+
+from models import Book
 
 
 app = FastAPI(title="First application",
@@ -13,6 +17,26 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+class BookResponse(BaseModel):
+    title: str
+    author: str
+
+@app.get("/allbooks")
+async def read_all_books() -> list[BookResponse]:
+    return [
+        {
+            "id": 1,
+            "title": "1984",
+            "author": "George Orwell",
+        },
+        {
+            "id": 2,
+            "title": "The Great Gatsby",
+            "author": "F. Scott Fitzgerald",
+        }
+    ]
+
 
 @app.get("/book/{book_id}")
 async def read_book(book_id: int):
@@ -37,3 +61,20 @@ async def reaad_author(author_id: int):
         "author_id": author_id,
         "name": "Ernest Hemingway"
     }
+
+@app.post("/book")
+async def create_bool(book: Book):
+    return book
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "message": "Oops! Something went wrong"
+        }
+    )
+
+@app.get("/error_endpoint")
+async def raise_exception():
+    raise HTTPException(status_code=400)
